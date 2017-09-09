7ba7872e565d3ee0ce0b89f3f6f77629efa141df
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-09-09 03:44:27 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,21 +4,20 @@ def valid_move?(board, index)
   if index.between?(1,9) == false
     return false
   elsif position_taken?(board, index) == true
-  		return true
-    else
-      return false
-    end
-    
+  	return true
+  else
+    return false
+  end
+
 end
 
 def position_taken?(board, index)
 
 	index -= 1
 
-	if board[index] == " " || ""
+	if board[index] == " " or board[index] == ""
 		return true
-  else
-    return false
+  	else
+    	return false
 	end
-
 end
