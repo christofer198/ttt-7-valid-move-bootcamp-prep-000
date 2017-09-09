a10ae684fae93c83bbbc051294cf2d2497689a8a
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-09-09 03:54:26 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,11 +2,11 @@
 def valid_move?(board, index)
 
   if index.between?(1,9) == false
-    return false
+    return true
   elsif position_taken?(board, index) == true
   	return true
   end
-  
+
 end
 
 def position_taken?(board, index)
