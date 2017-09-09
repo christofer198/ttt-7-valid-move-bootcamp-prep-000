a2287fcb84fb549bd35ed08d3167b530bc18b3da
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-09-09 04:14:48 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
 
-  #if index.between?(1,9) == false
-  #  return false
-  #end
+  if index.between?(1,9) == false
+    return false
+  end
 end
